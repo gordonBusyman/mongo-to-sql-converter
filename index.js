12fb9fcfafc95bc45be7a0ee5cba9094f3ff084b
Ivan Tarbakou <ivantarbakou@gmail.com>
2019-10-08 18:39:51 +0300

    Added JSDoc description in entrypoint

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
 const converter = require('./src/converter')
 
-var convertToSQL = (input, removeUnderscoreBeforeID = false) => {
+/**
+ *
+ * @param {string} input
+ * @param {boolean} removeUnderscoreBeforeID
+ *
+ * @returns {string}
+ * @description Convert MongoDB query to SQL query. removeUnderscoreBeforeID flag, if true will convert _id to id, default false
+ */
+const convertToSQL = (input, removeUnderscoreBeforeID = false) => {
   if (typeof input !== 'string') {
     throw new Error('Input must be type string')
   }
